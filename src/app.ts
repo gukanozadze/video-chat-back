6c8d8f600e3bb8e979f83f414e29ebec213aa264
Guka Nozadze <gukanozadze@gmail.com>
2021-08-10 19:25:34 +0400

    react app

--- src/app.ts
+++ src/app.ts
@@ -15,4 +15,28 @@ const io = new Server(httpServer, {
 
 app.use(cors())
 
-httpServer.listen(3000)
+const PORT = process.env.PORT || 8000
+
+app.get("/", (req, res) => {
+  res.send("Server is running")
+})
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id)
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callended")
+  })
+
+  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("calluser", { signal: signalData, from, name })
+  })
+
+  socket.on("answercall", (data) => {
+    io.to(data.to).emit("callaccepted", data.signal)
+  })
+})
+
+httpServer.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
